c61999016529a5e93f065e89a84083a3ecc30ec0
Muhammad Ali <amuhammad161@gmail.com>
2022-04-12 21:32:58 +0500

    added search option

--- src/App.jsx
+++ src/App.jsx
@@ -9,20 +9,48 @@ const SEARCH_API =
 
 function App() {
 	const [movies, setMovies] = useState([]);
+	const [searchTerm, setSearchTerm] = useState("");
 
 	useEffect(() => {
-		fetch(FEATURED_API)
+		getMovies(FEATURED_API);
+	}, []);
+
+	const getMovies = (API) => {
+		fetch(API)
 			.then((res) => res.json())
 			.then((data) => {
 				setMovies(data.results);
 			});
-	}, []);
+	}
+
+	const handleOnSubmit = (e) => {
+		e.preventDefault();
+		if(searchTerm) {
+		getMovies(SEARCH_API+searchTerm);
+		}
+	};
+
+	const handleOnChange = (e) => {
+		setSearchTerm(e.target.value);
+	};
 
 	return (
+	<>
+		<header>
+			<form onSubmit={handleOnSubmit}>
+				<input 
+				className="search" 
+				type="search" 
+				placeholder="Search..." 
+				value={searchTerm} 
+				onChange={handleOnChange} />
+			</form>
+			</header>
 		<div className="movie-container">
 			{movies.length > 0 &&
 				movies.map((movie) => <Movie key={movie.id} {...movie} />)}
 		</div>
+	</>
 	);
 }
 
--- src/components/movie.jsx
+++ src/components/movie.jsx
@@ -1,12 +1,22 @@
 const IMG_API = "http://image.tmdb.org/t/p/w1280";
 
+const setVoteClass = (vote) => {
+  if(vote>=8) {
+    return "green";
+  } else if(vote>=6) {
+    return "orange";
+  } else {
+    return "red";
+  }
+};
+
 const Movie = ({ title,poster_path,overview,vote_average }) => 
   (
   <div className="movie">
-    <img src={IMG_API + poster_path} alt={title} />
+    <img src={poster_path ? IMG_API + poster_path:"https://images.unsplash.com/photo-1560109947-543149eceb16?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=735&q=80"} alt={title} />
     <div className="movie-info">
       <h3>{title}</h3>
-      <span>{vote_average}</span>
+      <span className={`tag ${setVoteClass(vote_average)}`}>{vote_average}</span>
     </div>
 
     <div className="movie-over">
